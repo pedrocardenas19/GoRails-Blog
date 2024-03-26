e6611c39885af00b04c1f0f6a734678f795f2b22
Pedro Cardenas <pedrocardenasrestrepo@gmail.com>
2024-03-26 01:09:56 -0500

    add published_at

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -3,10 +3,11 @@ class BlogPostsController < ApplicationController
     before_action :authenticate_user!, except:[:index, :show]
     before_action :set_blog_post, except: [:index, :new, :create]
     def index
-        @blog_posts = BlogPost.all
+        @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
     end
 
     def show
+
     end
 
     def new
@@ -28,6 +29,7 @@ class BlogPostsController < ApplicationController
 
     def update
       if @blog_post.update(blog_post_params)
+        redirect_to @blog_post
       else
         render :edit, status: :unprocessable_entity
       end
@@ -43,11 +45,11 @@ class BlogPostsController < ApplicationController
     private
 
     def blog_post_params
-      params.require(:blog_post).permit(:title, :body)
+      params.require(:blog_post).permit(:title, :body, :published_at)
     end
 
     def set_blog_post
-      @blog_post = BlogPost.find(params[:id])
+      @blog_post = user_signed_in? ? BlogPost.find(params[:id]) : BlogPost.published.find(params[:id])
     rescue ActiveRecord::RecordNotFound
         redirect_to root_path
     end
